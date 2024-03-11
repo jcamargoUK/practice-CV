3b1747a0887f453869c794871d003a2f50c6c6ed
Julio Camargo <jcamargo@hotmail.co.uk>
2024-03-11 21:28:22 +0000

    added tailwind to the project, all initial set up complete.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //you can add other file types in the content, for example if you us typescript, you can ts, or jsx, or txs
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
